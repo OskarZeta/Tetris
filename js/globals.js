01bbeb2a2c174a256b55263f807376559cbf2b87
Juan <dw4h@yandex.ru>
2019-08-13 17:27:40 +0300

    Got rid of some of the 'magic numbers'

--- js/globals.js
+++ js/globals.js
@@ -1,3 +1,5 @@
 export let gameWidth = 12;
 export let gameHeight = 16; 
+export let movementSpeed = 300;
+export let loopRate = 500;
 export const figureTypes = ['I', 'J', 'L', 'O', 'S', 'T', 'Z'];
