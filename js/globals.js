a725fe9fda8ccd0db92d5d55f9e86b7a1313a34f
Juan <dw4h@yandex.ru>
2019-08-15 13:01:40 +0300

    Updated global variables

--- js/globals.js
+++ js/globals.js
@@ -1,5 +1,5 @@
 export let gameWidth = 12;
 export let gameHeight = 16; 
-export let movementSpeed = 300;
-export let loopRate = 500;
+export let movementSpeed = 500;
+export let loopRate = 400;
 export const figureTypes = ['I', 'J', 'L', 'O', 'S', 'T', 'Z'];
